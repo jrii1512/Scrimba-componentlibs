e53c74fff30b6d02e59055dc1424a10cc670f938
riimalaj <jriimala@gmail.com>
2023-10-29 15:53:08 +0200

    Color scheme and margin fixes

--- src/components/Badges/badges.jsx
+++ src/components/Badges/badges.jsx
@@ -3,22 +3,37 @@ import Badge from "./badge";
 //import classNames from "classnames";
 
 export default function Badges() {
-    const colors = ['gray', 'red', 'yellow', 'green', 'blue', 'indigo', 'purple', 'pink'];
+  const colors = [
+    "gray",
+    "red",
+    "yellow",
+    "green",
+    "blue",
+    "indigo",
+    "purple",
+    "pink",
+  ];
   return (
-    <section id = "badges" className = "badges--container">
+    <div id="badges" className="badges-container">
       <h1 className="badge-title">Badges</h1>
-      
-      <h3 className = "title square--title">SQUARE</h3>
-        <div className = "square--container">
-            {colors.map( color => <Badge key={color} variant = 'square' color={color}>Badge</Badge>)}
-        </div>
 
-        <h3 className="title pill--title">PILL</h3>
-            <div className="pill--container">
-            {colors.map(color => <Badge key={color} variant='pill' color={color}>Badge</Badge>)}
-            </div>
+      <h3 className="title square-title">SQUARE</h3>
+      <div className="square-container">
+        {colors.map((color) => (
+          <Badge key={color} variant="square" color={color}>
+            Badge
+          </Badge>
+        ))}
+      </div>
 
-    
-    </section>
-  );
+      <h3 className="title pill-title">PILL</h3>
+      <div className="pill-container">
+        {colors.map((color) => (
+          <Badge key={color} variant="pill" color={color}>
+            Badge
+          </Badge>
+        ))}
+      </div>
+    </div>
+  )
 }
